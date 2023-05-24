173daa357f435b50dd2612bab03f818ac329835f
root <JaggerVanW@gmail.com>
2023-05-24 16:07:40 -0700

    Adding C file that prints without puts or printf

---
+++ hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Printing out a quote without printf or puts function
+ */
+
+int main(void)
+{
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	return (1);
+}
