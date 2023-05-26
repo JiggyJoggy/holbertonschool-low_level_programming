40fe456cc6459bca084e7c9620d781a79064afe0
root <JaggerVanW@gmail.com>
2023-05-26 14:52:14 -0700

    Printing the alphabet in lowercase and uppercase

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * description: printing the alphabet using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char start = 'a';
+	char end = 'z';
+
+	char letter = start;
+
+	while (letter <= end)
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	letter = start;
+
+	while (letter <= end)
+	{
+		putchar(toupper(letter));
+		letter++;
+	}
+
+	putchar('\n');
+	return (0);
+}
