579a6a029feebe6fca808bc95eb295a2f4c1fcd9
root <JaggerVanW@gmail.com>
2023-05-26 14:22:02 -0700

    Printing the alphabet in lowercase

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * description:
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void) {
+	char alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+	putchar(alphabet);
+
+	return (0);
+}
