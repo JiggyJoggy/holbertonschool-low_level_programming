d5f405e987017ca24ba260e86c6d888e219a7606
root <JaggerVanW@gmail.com>
2023-05-28 13:06:32 -0700

    Gets the last digit of a number

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - entry point
+ *
+ * description: gets the last digit of a number
+ * @n: gets an integer
+ *
+ * Return: int
+ */
+
+int _abs(int n)
+{
+	return (n % 10);
+}
