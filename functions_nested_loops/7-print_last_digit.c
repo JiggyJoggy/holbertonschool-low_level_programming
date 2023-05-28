4453d0115902f5a30418b576e5fae3f2e295b741
root <JaggerVanW@gmail.com>
2023-05-28 13:13:25 -0700

    Prints the last digit of a number

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -12,5 +12,14 @@
 
 int print_last_digit(int n)
 {
-	return (n % 10);
+	if (n >= 0)
+	{
+		_putchar(n % 10);
+		return (n % 10);
+	}
+	else
+	{
+		_putchar(n * 1 % 10);
+		return(n * 1 % 10);
+	}
 }
