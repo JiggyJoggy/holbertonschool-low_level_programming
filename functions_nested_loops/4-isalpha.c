ac4f1bd94b6ca437b9e88c2c0f227643413a8023
root <JaggerVanW@gmail.com>
2023-05-28 12:38:58 -0700

    checking for lowercase and uppercase char

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isalpha - entry point
+ *
+ * description: checks for lowercase and uppercase characters
+ * @c: c is getting a char
+ *
+ * Return: 1 if lowercase or uppercase, otherwise 0
+ */
+
+int _isalpha(int c)
+{
+	if (c <= 90 && c >= 65 || c <= 122 && c >= 97)
+	{
+		return (1);
+	}
+	return (0);
+}
