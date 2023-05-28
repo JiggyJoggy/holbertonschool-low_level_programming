3982403925db77783f0d6c9b1a70457903bfd7b0
root <JaggerVanW@gmail.com>
2023-05-28 10:13:31 -0700

    Printing the alphabet from main.h

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * description: printing alphabet
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,1 +1,19 @@
 int _putchar(char c);
+
+void print_alphabet(void);
+
+int main(void)
+{
+	char start = 'a';
+	char end = 'z';
+
+	char letter = start;
+
+	while (letter <= end)
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+	return (0);
+}
