d86752dab41822bdf55f617160ef25d25de35371
root <JaggerVanW@gmail.com>
2023-05-27 17:09:20 -0700

    Printing '_putchar'

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * description: printing out _putchar
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	printf("_putchar");
+
+	return (0);
+}
