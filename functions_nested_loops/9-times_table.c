66048fb238eb8374d3abaa9a1fca379b9109c83c
root <JaggerVanW@gmail.com>
2023-05-29 13:46:09 -0700

    Printing a times table (9)

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,85 @@
+#include "main.h"
+
+/**
+ * print_num - entry point
+ *
+ * description: prints num with comma and spaces
+ * @n: int to print
+ *
+ * Return: void
+ */
+
+void print_num(int n)
+{
+	_putchar('0' + n);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(' ');
+}
+
+/**
+ * print_double - entry point
+ *
+ * description: prints double digit nums
+ * @n: int to print
+ * @last: last multiple
+ *
+ * Return: void
+ */
+
+void print_double(int n, int last)
+{
+	_putchar('0' + (n / 10));
+	_putchar('0' + (n % 10));
+
+	if (n == last)
+	{
+		return;
+	}
+
+	_putchar(',');
+	_putchar(' ');
+}
+
+/**
+ * times_table - entry point
+ *
+ * Description: print times table
+ *
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int multiple = 0, increment = 1;
+
+	while (multiple <= 9)
+	{
+		int curr_num = multiple * increment;
+
+		print_num(0);
+
+		while (increment <= 9)
+		{
+			int curr_num = multiple * increment;
+
+			if (curr_num > 9)
+			{
+				print_double(curr_num, last_num);
+				increment++;
+				continue;
+			}
+
+			if ((curr_num == last_num && curr_num != 0) || (increment == 9))
+			{
+				_putchar('0' + cuur_num);
+				break;
+			}
+			print_num(curr_num);
+			increment++;
+		}
+		_putchar('\n');
+		increment = 1;
+		multiple++;
+	}
+}
