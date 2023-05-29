774793664e3ed8e7486198426605259b3e316d9c
root <JaggerVanW@gmail.com>
2023-05-28 20:03:39 -0700

    Times table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * times_table - entry point
+ *
+ * description: printing a times table from 0 to 9
+ *
+ * Return: void
+ */
+
+void times_table(void);
+{
+	int n;
+
+	for (int i = 1; i <= 9; i++)
+	{
+		_putchar("%d * %d = %d", n, i, n * i)
+	}
+	return (void)
+}
