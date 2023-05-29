c3bf021713cb7c0d8fb327294885e15488d6345d
root <JaggerVanW@gmail.com>
2023-05-28 20:41:45 -0700

    Sorting n to 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include <main.h>
+#include <stdio.h>
+
+/**
+ * print_to_98 - entry point
+ *
+ * description: prints all natural numbers from n to 98
+ * @n: taking a int
+ *
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	int end_num;
+
+	end_num = 98;
+
+	while (n <= end_num)
+	{
+		_putchar('n');
+		n++;
+	}
+	return (qsort(n));
+}
