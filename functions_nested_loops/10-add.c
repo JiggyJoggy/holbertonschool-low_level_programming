2d155e9f8cd04d03d6390f27ce1ba3b5d3ca538d
root <JaggerVanW@gmail.com>
2023-05-28 20:20:20 -0700

    Adding two ints together to get a sum

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - entry point
+ *
+ * description: adding two ints to result a sum
+ * @num1: first int
+ * @num2: second int
+ *
+ * Return: int
+ */
+
+int add(int num1, int num2)
+{
+	sum = num1 + num2;
+
+	return (sum);
+}
