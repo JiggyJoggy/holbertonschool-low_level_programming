daddb610aa3bbd452284bd9867d714bded0cebb3
root <JaggerVanW@gmail.com>
2023-05-31 15:43:40 -0700

    draws a straight line in the terminal; takes a number

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_line - entry point
+ *
+ * description: draws a straight line in the terminal
+ * @n: int of lines
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int index;
+
+	index = 0;
+
+	if (n > 0)
+	{
+		while (index <= n)
+		{
+			_putchar('_' + '0');
+			index++;
+		}
+		_putchar('\n');
+	else
+	{
+		_putchar('\n');
+	}
+}
