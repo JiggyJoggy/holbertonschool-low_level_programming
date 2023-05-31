6f4a7336ad8fe602c66da230bd19039d5d074cae
root <JaggerVanW@gmail.com>
2023-05-31 13:57:05 -0700

    Multiples two numbers together

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - entry pont
+ *
+ * description: multiples two integers
+ * @a: num1
+ * @b: num2
+ *
+ * Return: Always 0 (Success)
+ */
+
+int mul(int a, int b)
+{
+	int sum;
+
+	sum = a * b;
+
+	return (sum);
+}
