82e86a9706ef5f84f6a1caaf0d3ac46a87bb64e9
root <JaggerVanW@gmail.com>
2023-05-31 14:20:21 -0700

    Prints numb 0 to 9 without 2 and 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - entry point
+ *
+ * description: print 0 to 9 without 2 and 4, followed by a new line
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int index;
+
+	index = 0;
+
+	while (index <= 9)
+	{
+		if (index == 2 || index == 4)
+		{
+			continue;
+		}
+		_putchar(index + '0');
+		index++;
+	}
+	_putchar('\n');
+}
