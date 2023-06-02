0216b5c37ff356e997ce50e55a44ce34f634c21d
root <JaggerVanW@gmail.com>
2023-06-02 14:35:46 -0700

    Printing 1 to 100, multiples of 3 print Fizz, multiples of 5 print Buzz, multiples of both print FizzBuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -14,17 +14,17 @@ int main(void)
 
 	for (index = 1; index <= 100; index++)
 	{
-		if (index % 3 == 0)
+		if (index % 3 == 0 && index % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (index % 3 == 0)
 		{
 			printf("Fizz");
 		}
 		else if (index % 5 == 0)
 		{
 			printf("Buzz");
-		}
-		else if (index % 3 == 0 && index % 5 == 0)
-		{
-			printf("FizzBuzz)");
 		}
 		else
 		{
