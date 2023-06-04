72d16521476afa12d92ffa74d9c6bcd7bc100980
root <JaggerVanW@gmail.com>
2023-06-03 18:23:22 -0700

    Returns the length of a string

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - entry point
+ *
+ * description: returns the length of a string
+ * @s: takes a char pointer
+ *
+ * Return: length of a string
+ */
+
+int _strlen(char *s)
+{
+	return (_strlen(*s));
+}
