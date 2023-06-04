f84d84e9e85671eb1f94de668bc561a81a37db76
root <JaggerVanW@gmail.com>
2023-06-04 16:43:00 -0700

    Using pointers to reverse a string, arrays

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - entry point
+ *
+ * description: a function that reverses a string
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int count;
+
+	count = 0;
+
+	while (s[count] != '\0')
+	{
+		count++
+	}
+
+	for (count = count - 1; count >= 0; count--)
+	{
+		_putchar(s[count]);
+	}
+	_putchar('\n');
+}
