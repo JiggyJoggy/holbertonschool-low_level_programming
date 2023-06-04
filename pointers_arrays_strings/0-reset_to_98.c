4168a6851ecfd4180bad375e97b5f8166e86e1a8
root <JaggerVanW@gmail.com>
2023-06-03 18:00:31 -0700

    Pointer to an int, value is now 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - entry point
+ *
+ * description: taking a pinter to an int as a para; value 98
+ *@*n: taking an int pointer
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
