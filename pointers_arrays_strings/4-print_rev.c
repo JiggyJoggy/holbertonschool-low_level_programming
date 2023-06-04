ff6d042d431d8ebcb1e65d88053d3486e7e7ab0a
root <JaggerVanW@gmail.com>
2023-06-04 16:15:13 -0700

    Using pointers to reverse a string

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -11,16 +11,24 @@
 
 void print_rev(char *s)
 {
-	int count;
+	int count, index;
+	char *begin;
 	char ch = *s;
 
-	for (count = ch; *s != '\0'; count++)
+	begin = s;
+
+	while (count != '\0')
 	{
+		count++;
 		s++;
 	}
-	for (count = ch - 1; *s > count; count--)
+	for (index = 0; index < count; index++)
 	{
 		_putchar(ch);
+		if (s == begin)
+		{
+			break;
+		}
 		s--;
 	}
 	_putchar('\n');
