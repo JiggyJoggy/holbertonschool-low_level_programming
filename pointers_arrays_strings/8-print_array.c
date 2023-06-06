24c7334d12314743dddc55b56c43894e021d0397
root <JaggerVanW@gmail.com>
2023-06-05 17:01:29 -0700

    Printing out an array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_array - entry point
+ *
+ * description: an array of ints
+ * @a: the array arug
+ * @n: the num of int in that array
+ *
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int a[n];
+	int loop;
+
+	for (loop = 0; loop <= n; loop++)
+	{
+		printf("%d, ", a[n]);
+	}
+}
