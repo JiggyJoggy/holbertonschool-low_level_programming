79cfe8860e138f6b9b3a4c15774e3781059dec24
root <JaggerVanW@gmail.com>
2023-06-09 17:50:39 -0700

    copying a string, using bytes to see how much to copy

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strncpy - entry point
+ *
+ * description: copies a string
+ * @dest: where it'll end
+ * @src: where we get it
+ * @n: size of bytes
+ *
+ * Return: char
+ */
+
+char *strncpy(char *dest, char *src, int n)
+{
+	int index_s = 0;
+	int index_d = 0;
+
+	while (dest[index_d] != '\0')
+	{
+		index_d++;
+	}
+
+	for (; src[index_s] != '\0' && index_s < n; index_s++)
+	{
+		dest[index_d] = src[index_s];
+	}
+	dest[index_d] = '\0';
+
+	return (dest);
+}
