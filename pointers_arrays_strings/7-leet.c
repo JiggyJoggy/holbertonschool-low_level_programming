c3ee2b93e5ae3d35bb40f58be7fc6a708b089aaf
root <JaggerVanW@gmail.com>
2023-06-11 22:04:36 -0700

    encodes a string into 1337

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * leet - entry point
+ *
+ * description: encodes a string into 1337
+ * @c: takes a string char
+ *
+ * Return: char
+ */
+
+char *leet(char *c)
+{
+	char encode[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	int count, arr_index;
+
+	encode_len = 10;
+
+	for (count = 0; c[count] != '\0'; count++)
+	{
+		for (arr_index = 0; arr_index < encode_len; arr_index++)
+		{
+			if (c[count] == encode[arr_index])
+			{
+				c[count] = encode[arr_index];
+			}
+		}
+	}
+	return (c);
+}
