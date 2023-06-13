75223faf544987adaf36e4a4f544d3d2d8f9f2ef
root <JaggerVanW@gmail.com>
2023-06-12 21:48:39 -0700

    Prints a chessboard

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_chessboard - entry point
+ *
+ * description: prints the chessboard
+ * @a: an array
+ *
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int c1, c2;
+
+	for (c1 = 0; c1 < 8; c1++)
+	{
+		for (c2 = 0; c2 < 8; c2++)
+		{
+			_putchar(a[c1][c2]);
+		}
+	}
+}
