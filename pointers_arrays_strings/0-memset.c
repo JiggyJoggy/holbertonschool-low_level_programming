a79673535cddfc0fab27dea241f5094044e56c37
root <JaggerVanW@gmail.com>
2023-06-12 17:36:39 -0700

    Fills memory with a constant byte

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _memset - entry point
+ *
+ * desciprtion: a function that fills memory with a constant byte
+ * @s: area pointed
+ * @b: the constant byte
+ * @n: fills the first number bytes
+ *
+ * Return: char
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int count;
+
+	count = 0;
+
+	while (*s != '\0')
+	{
+		for (count = 0; s[count] < n; count++)
+		{
+			s[count] == '&';
+		}
+	}
+	return (s);
+}
