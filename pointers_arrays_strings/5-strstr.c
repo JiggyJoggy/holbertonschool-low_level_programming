f6beb8f3e159767cef522c9c73ccf81701a55d59
root <JaggerVanW@gmail.com>
2023-06-12 21:18:45 -0700

    locates a substring

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strstr - entry point
+ *
+ * description: function that locates a substring
+ * @haystack: where to look (char)
+ * @needle: what to find (char)
+ *
+ * Return: char
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack && *needle)
+	{
+		if (*haystack != *needle)
+		{
+			return (0);
+		}
+		haystack++;
+		needle++;
+	}
+	return (*needle == '\0')
+}
