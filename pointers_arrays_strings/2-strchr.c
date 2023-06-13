7330b2f9612ebd308e40bce01654ce9eb1113685
root <JaggerVanW@gmail.com>
2023-06-12 19:17:20 -0700

    Locates a character in a string

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - entry point
+ *
+ * description: locates a character in a string
+ * @s: the whole string
+ * @c: the char
+ *
+ * Return: char
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*str == c)
+		{
+			return ((char *) s);
+		}
+		s++;
+	}
+	return NULL;
+}
