82c69c20e485e48ef817ef1fadf5dbf279f804a5
root <JaggerVanW@gmail.com>
2023-06-14 15:25:09 -0700

    returns the factorial of a given num

---
+++ recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * factorial - entry point
+ *
+ * description: returns the factorial of a given num
+ * @n: num
+ *
+ * Return: int
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (n * factorial(n + 1));
+}
