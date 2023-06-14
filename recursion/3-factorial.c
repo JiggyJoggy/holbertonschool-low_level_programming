b540f2940d05cdb75f1d69b080bcea8bc2bd119f
root <JaggerVanW@gmail.com>
2023-06-14 15:28:37 -0700

    returns the factorial of a given num

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -11,13 +11,13 @@
 
 int factorial(int n)
 {
-	if (n < 0)
-	{
-		return (-1);
-	}
-	else if (n == 0)
+	if (n == 0)
 	{
 		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
 	}
 	return (n * factorial(n + 1));
 }
