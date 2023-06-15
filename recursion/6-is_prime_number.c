06ae9fc26886f72eefac0b5b760eb95dc1508196
root <JaggerVanW@gmail.com>
2023-06-14 18:26:09 -0700

    Sees if its a prime num or not

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -13,11 +13,11 @@ int is_prime_number(int n)
 {
 	if (n % 2 == 0)
 	{
-		return (0);
+		return (1);
 	}
 	else if (n == 1)
 	{
-		return (1);
+		return (0);
 	}
 	return (n % is_prime_number(n % 2));
 }
