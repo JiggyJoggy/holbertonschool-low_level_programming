456275af3e06e07483a293f10802d9dd2c6e6853
root <JaggerVanW@gmail.com>
2023-06-14 18:24:15 -0700

    Sees if its a prime num or not

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * is_prime_number - entry point
+ *
+ * description: checks if its a prime num
+ * @n: num
+ *
+ * Return: 1 if prime, otherwise 0
+ */
+
+int is_prime_number(int n)
+{
+	if (n % 2 == 0)
+	{
+		return (0);
+	}
+	return (n % is_prime_number(n % 2));
+}
