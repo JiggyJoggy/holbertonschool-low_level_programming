8561140bbd2a8cf01271b2b9e7d846adc3f682f9
root <JaggerVanW@gmail.com>
2023-06-26 17:28:10 -0700

    2 Dimensoinal array of integers

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -33,7 +33,7 @@ int **alloc_grid(int width, int height)
 	{
 		for (index_2 = 0; index_2 < height; index_2++)
 		{
-			ptr = ptr[index][index_2];
+			ptr[index][index_2];
 		}
 	}
 	ptr[index][index_2] = 0;
