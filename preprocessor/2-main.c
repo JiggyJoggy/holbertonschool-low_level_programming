a2ccf361fec133f7705b0ecd1ec1e2ba75d86e03
root <JaggerVanW@gmail.com>
2023-07-01 17:24:24 -0700

    File name

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,1 +1,17 @@
-#include
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * description: prints the name of the file it was
+ * compiled from
+ *
+ * Return: 0 Success
+ */
+
+int main(void)
+{
+	printf("%s\n",__FILE__);
+
+	return (0);
+}
