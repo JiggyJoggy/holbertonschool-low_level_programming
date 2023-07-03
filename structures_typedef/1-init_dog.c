a6c0a1035d04c65db949bf5a76d74b19bc51e1fe
root <JaggerVanW@gmail.com>
2023-07-02 17:40:34 -0700

    Initialize a variable of type struct dog

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "dog.h"
 
 /**
@@ -18,10 +17,10 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
 	{
-		return (NULL);
+		return;
 	}
 
-	d.->name = name;
-	d.->age = age;
-	d.->owner = owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
