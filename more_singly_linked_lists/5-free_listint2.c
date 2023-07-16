14cf036372a63105b4385ea5b440afb796aad4e6
root <JaggerVanW@gmail.com>
2023-07-15 19:36:42 -0700

    Frees a list

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - entry point
+ *
+ * description: frees a list
+ * @head: points to the pointer of head
+ *
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	if (head == NULL)
+	{
+		return;
+	}
+
+	while (*head != NULL)
+	{
+		current = *head;
+		*head = (*head)->next;
+		
+		free(current);
+	}
+	*head = NULL;
+}
