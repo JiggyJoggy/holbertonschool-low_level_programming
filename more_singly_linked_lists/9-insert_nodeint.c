07f308a8799d01b1d4e0979b3d471cadf7226d53
root <JaggerVanW@gmail.com>
2023-07-15 20:47:35 -0700

    Inserts a new node at a given position

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -28,6 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		newNode->next = NULL;
 		*head = newNode;
 		return (newNode);
+
 	current = *head;
 	prev = NULL;
 
