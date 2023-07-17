cffea4b516a23353bcbe36573a14d562563926ab
root <JaggerVanW@gmail.com>
2023-07-17 09:22:00 -0700

    Creates a file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * create_file - entry point
+ *
+ * description: creates a file
+ * @filename: pointer to the filename
+ * @text_content: pointer to your content
+ *
+ * Return: -1 if fails, else return 1 on success
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int file_desc;
+
+	if (!text_content)
+	{
+		text_content = "";
+	}
+
+	file_desc = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
+	if (file_desc < 0)
+	{
+		return (-1);
+	}
+
+	if (text_context)
+	{
+		write(file_desc, text_content, strlen(text_content));
+	}
+	close(file_desc);
+
+	return (1);
+}
