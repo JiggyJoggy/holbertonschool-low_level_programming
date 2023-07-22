ba32538f1342650badf55cd637a2c182c4c14a27
root <JaggerVanW@gmail.com>
2023-07-22 13:50:32 -0700

    Adds a new node at the end of a list

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -38,7 +38,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		temp = temp->next;
 	}
 
-	trmp->next = newNode;
+	temp->next = newNode;
 	newNode->prev = temp;
 
 	return (newNode);
