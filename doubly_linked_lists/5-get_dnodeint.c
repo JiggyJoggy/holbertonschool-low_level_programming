01e69758472f7ce44b49c4a1c6dec4bad8a7d342
root <JaggerVanW@gmail.com>
2023-07-23 13:18:38 -0700

    Returns the nth node of a linked list

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -36,8 +36,8 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 	i = 0;
 	current = head;
-	
-	while (i <index && current != NULL)
+
+	while (i < index && current != NULL)
 	{
 		current = current->next;
 		i++;
