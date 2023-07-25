4ece737912d78140f6c870911ec98a87ab46768e
root <JaggerVanW@gmail.com>
2023-07-24 18:46:07 -0700

    Deletes a hash table

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - entry point
+ *
+ * description: deletes a hash table
+ * @ht: hash table
+ *
+ * Return: void
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *head;
+	hash_node_t *temp;
+	unsigned long int index;
+
+	if (!ht)
+	{
+		return;
+	}
+
+	for (index = 0; index < ht->size; index++)
+	{
+		headh = ht->array[index];
+
+		while (head)
+		{
+			temp = head;
+			head = head->next;
+
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
