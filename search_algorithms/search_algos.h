21095ba6d984ee96f22457b22fc24bf474468a5d
root <JaggerVanW@gmail.com>
2023-08-23 08:14:36 -0700

    test

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -2,8 +2,9 @@
 #define SEARCH_ALGOS_H
 #include <stdio.h>
 #include <stddef.h>
+#include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-void print_array(int *array, size_t left, size_t right);
+void print_array(const int *array, size_t size);
 #endif
